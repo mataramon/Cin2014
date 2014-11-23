febc6153d5ed6faab09b7d44e92c37672d7c647b
Angel Jovany Figueroa Ypiña <leofigy@Angels-MacBook-Pro.local>
2014-11-23 13:50:27 -0600

    basic threads example

---
+++ basic_threads.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Angel Jovany Figueroa Ypiña on 11/6/14.
+//  Copyright (c) 2014 Angel Jovany Figueroa Ypiña. All rights reserved.
+//
+
+#include <iostream>
+#include <thread>
+#include <vector>
+
+void hello(){
+    std::cout << "Hello Thread " << std::this_thread::get_id() << std::endl;
+}
+// Main execution
+int main(){
+    
+    // Here you create a vector of threads
+    std::vector<std::thread> threads;
+    // Filling the vector of threads (as it is a pool of threads)
+    for(int i = 0; i < 10; ++i){
+        threads.push_back(std::thread(hello));
+    }
+    // For each thread in vector
+    for(auto& thread : threads){
+        // Start the thread
+        thread.join();
+    }
+    
+    return 0;
+}
