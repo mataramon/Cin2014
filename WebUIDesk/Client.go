82d116f8682473654f45ca8765907aec38ce4a7c
Angel Figueroa <leofigy@gmail.com>
2014-11-29 23:49:34 -0600

    done it

--- WebUIDesk/Client.go
+++ WebUIDesk/Client.go
@@ -3,22 +3,85 @@ package main
 import (
         "flag"
         "fmt"
+        "os"
+        _"net/http"
+        "log"
+        "io/ioutil"
         )
 
+/*******************************************/
+// Web page data structure
+
+type WebPage struct {
+    Title string
+    Body []byte
+}
+
+func (self *WebPage) save() error {
+    filename := self.Title + ".tmpl"
+    return ioutil.WriteFile(filename, self.Body,0600)  
+}
+
+func loadWebPage(title string) (*WebPage, error){
+    log.Println("Loading web page tempalte file: ", title)
+
+    filename := title + ".tmpl"
+    body, err := ioutil.ReadFile(filename)
+
+    if err != nil{
+        return nil, err
+    }
+
+    return &WebPage{Title: title, Body: body}, nil
+}
+
+/**************************************/
+// CLIENT OBJECT 
+// This object download the list of coordenates
 type Client struct{
-    Server string
+    Server  string   // Server URL location 
+    LogFile string   // log file name e.g myclient.log
+    Template string  // Template name for the front end app e.g ui.tmpl 
+
 }
 
+/* Parser the command line parameters*/
 func (self *Client) init() {
-    var tmp = flag.String("server", "http://162.243.155.132:3000", "This is the server location to download all files")
+    // Parameters
+    var server   = flag.String("server",  "http://162.243.155.132:3000", "This is the server location to download all files")
+    var logfile  = flag.String("logfile", "client.log", "Please entry the log location") 
+    var template = flag.String("template", "ui", "Insert the web page template to render eg ui")
+
     flag.Parse()
-    self.Server = *tmp;
+    self.Server  = *server
+    self.LogFile = *logfile
+    self.Template= *template
+
+    // Logger
+    log_file, err := os.OpenFile(self.LogFile, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+    if err != nil{
+        fmt.Println("Cannot create log file ")
+    }
+    //defer log_file.Close()
+    log.SetOutput(log_file)
+
+
 }
 
+/* Shows the config to be used by the server */
 func (self *Client) config() {
-    fmt.Println("----- Map Client ----")
-    fmt.Println("-> Remote Server: ", self.Server)
+    log.Println("*******************************************")
+    log.Println("Starting Map Client application")
+    log.Println("Remote Server: ", self.Server)
 }
+
+func (self *Client) Download() {
+
+
+}
+
+
+/*END OF ALL DATA STRUCTURES */
 
 func main() {
     // Load config file
@@ -26,5 +89,15 @@ func main() {
     Rx.init()
     Rx.config()
 
+    Web, err := loadWebPage(Rx.Template)
+    if err != nil{
+        log.Fatalln("Cannot continue error loading the template file", Rx.Template, err)
+    }
+    fmt.Println(string(Web.Body))
+
+
+
+    log.Println("All operations done")
+    log.Println("*******************************************")
 
 }
