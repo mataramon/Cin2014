eca90c31e79b64f4615547819ea4f5b49117242a
Angel Figueroa <leofigy@gmail.com>
2015-01-04 01:16:04 -0600

    tested on linux x64_x86 + OS X 10.9

--- course/week3/compile.sh
+++ course/week3/compile.sh
@@ -2,23 +2,24 @@ echo "--------COMPILING----------";
 echo "Determining your operating system"
 OS="$(uname)";
 
-if [ -e Linux_wave.exec ]
+if [ -e "$OS"_wave.exec ]
     then
         rm "$OS"_wave.exec
     fi
-    
+
+echo "Compilation"
 if [ $OS = "Linux" ]
 then
-    echo "Compilation"
     g++ main.cpp soundhandler.cpp -std=c++11 -o "$OS"_wave.exec
-    echo "Execution privs"
-    chmod +x "$OS"_wave.exec;
 fi
 
 if [ $OS = "Darwin" ]
-then
-    echo $OS;
+    then
+    clang++ main.cpp soundhandler.cpp -std=c++11 -stdlib=libc++-o "$OS"_wave.exec;
 fi
+
+echo "Execution privs"
+chmod +x "$OS"_wave.exec;
 
 echo "------ DONE -----";
 echo "Executing: "
