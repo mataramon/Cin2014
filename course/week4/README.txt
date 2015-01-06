f310a56cf499e76b87d116d2d5bf138cae2b747c
angel figueroa <leofigy@gmail.com>
2015-01-06 12:22:38 -0600

    gui examples python

---
+++ course/week4/README.txt
@@ -0,0 +1,5 @@
+Semana de aplicaciones
+
+1- Como llamar codigo en c++ mediante una libreria dinamica (dll - windows)
+   desde un programa en python.
+
---
+++ course/week4/gui.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'dialog.ui'
+#
+# Created: Tue Jan  6 01:57:53 2015
+#      by: pyside-uic 0.2.15 running on PySide 1.2.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(366, 235)
+        self.findButton = QtGui.QPushButton(Dialog)
+        self.findButton.setGeometry(QtCore.QRect(240, 20, 111, 31))
+        self.findButton.setObjectName("findButton")
+        self.findText = QtGui.QLineEdit(Dialog)
+        self.findText.setGeometry(QtCore.QRect(20, 20, 211, 31))
+        self.findText.setObjectName("findText")
+        self.textEdit = QtGui.QTextEdit(Dialog)
+        self.textEdit.setGeometry(QtCore.QRect(20, 70, 331, 151))
+        self.textEdit.setObjectName("textEdit")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+        self.findButton.setText(QtGui.QApplication.translate("Dialog", "Find", None, QtGui.QApplication.UnicodeUTF8))
